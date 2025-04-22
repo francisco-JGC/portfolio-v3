d5cd0eae2ffc0bb8d6692eb10b9ee28d05c4a0cc
francisco-JGC <jjjchico1@gmail.com>
2025-04-22 15:14:43 -0600

    feat: restructure App component to include Header, HomePage, and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css'
+import { Footer } from './components/footer'
+import { Header } from './components/header'
+import { HomePage } from './pages/home/page'
 
 function App() {
   return (
     <main className='chico-dev'>
+      <Header />
 
+      <div>
+        <HomePage />
+      </div>
+
+      <Footer />
     </main>
   )
 }
