b97258f6f809f18e229c6e4762869e2b523f00ce
francisco-JGC <jjjchico1@gmail.com>
2025-04-23 14:52:21 -0600

    feat: add ABOUT_EXPERIENCE_ITEMS data structure for experience highlights

---
+++ src/data/aboutExperienciaItem.ts
@@ -0,0 +1,22 @@
+export const ABOUT_EXPERIENCE_ITEMS = [
+  {
+    title: "+3 Year",
+    subject: "Of Experience",
+    icon: "fa-solid fa-briefcase",
+  },
+  {
+    title: "+10 Projects",
+    subject: "Projects I have worked on",
+    icon: "fa-solid fa-diagram-project",
+  },
+  {
+    title: "+5 Techs",
+    subject: "Mastered technologies",
+    icon: "fa-solid fa-code",
+  },
+  {
+    title: "Remote Ready",
+    subject: "Open to remote opportunities",
+    icon: "fa-solid fa-globe",
+  },
+];
