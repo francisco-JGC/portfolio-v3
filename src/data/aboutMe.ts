af303f5cb275900206a2c604760e5d583755aee3
francisco-JGC <jjjchico1@gmail.com>
2025-04-24 13:43:27 -0600

    feat: add aboutMe data file with soft skills and technology details

---
+++ src/data/aboutMe.ts
@@ -0,0 +1,169 @@
+import angular from "../assets/technologies/angular.webp";
+import react from "../assets/technologies/react.webp";
+import node from "../assets/technologies/node.webp";
+import nest from "../assets/technologies/nest.webp";
+import nginx from "../assets/technologies/nginx.webp";
+import aws from "../assets/technologies/aws.webp";
+import git from "../assets/technologies/git.webp";
+import github from "../assets/technologies/github.webp";
+import html from "../assets/technologies/html.webp";
+import css from "../assets/technologies/css.webp";
+import ts from "../assets/technologies/ts.webp";
+import tailwind from "../assets/technologies/tailwind.webp";
+import js from "../assets/technologies/js.webp";
+import zustand from "../assets/technologies/zustand.webp";
+import firebase from "../assets/technologies/firebase.webp";
+import typeorm from "../assets/technologies/typeorm.webp";
+import postgresql from "../assets/technologies/postgresql.webp";
+
+export const SOFT_SKILLS = [
+  "Critical thinking",
+  "Problem-solving",
+  "Effective communication",
+  "Team collaboration",
+  "Autonomy and accountability",
+  "Adaptability",
+  "Technical empathy",
+];
+
+export const TECHNOLOGIES = [
+  {
+    name: "Node.js",
+    img: node,
+    style: {
+      backgroundColor: "#8CC84B",
+      color: "#fff",
+    },
+  },
+  {
+    name: "NestJS",
+    img: nest,
+    style: {
+      backgroundColor: "#E0234E",
+      color: "#fff",
+    },
+  },
+  {
+    name: "React.js",
+    img: react,
+    style: {
+      backgroundColor: "#61DAFB",
+      color: "#000",
+    },
+  },
+  {
+    name: "Zustand",
+    img: zustand,
+    style: {
+      backgroundColor: "#F44E3B",
+      color: "#fff",
+    },
+  },
+  {
+    name: "Firebase",
+    img: firebase,
+    style: {
+      backgroundColor: "#FFCA28",
+      color: "#000",
+    },
+  },
+  {
+    name: "PostgreSQL",
+    img: postgresql,
+    style: {
+      backgroundColor: "#336791",
+      color: "#fff",
+    },
+  },
+  {
+    name: "TypeORM",
+    img: typeorm,
+    style: {
+      backgroundColor: "#F44E3B",
+      color: "#fff",
+    },
+  },
+  {
+    name: "Nginx",
+    img: nginx,
+    style: {
+      backgroundColor: "#009639",
+      color: "#fff",
+    },
+  },
+  {
+    name: "Git",
+    img: git,
+    style: {
+      backgroundColor: "#F05032",
+      color: "#fff",
+    },
+  },
+  {
+    name: "GitHub",
+    img: github,
+    style: {
+      backgroundColor: "#181717",
+      color: "#fff",
+    },
+  },
+  {
+    name: "HTML5",
+    img: html,
+    style: {
+      backgroundColor: "#E34F26",
+      color: "#fff",
+    },
+  },
+  {
+    name: "CSS3",
+    img: css,
+    style: {
+      backgroundColor: "#1572B6",
+      color: "#fff",
+    },
+  },
+  {
+    name: "JavaScript",
+    img: js,
+    style: {
+      backgroundColor: "#F7DF1E",
+      color: "#000",
+    },
+  },
+  {
+    name: "TypeScript",
+    img: ts,
+    style: {
+      backgroundColor: "#007ACC",
+      color: "#fff",
+    },
+  },
+  {
+    name: "Tailwind CSS",
+    img: tailwind,
+    style: {
+      backgroundColor: "#06B6D4",
+      color: "#fff",
+    },
+  },
+];
+
+export const TECHNOLOGIES_COMING_SOON = [
+  {
+    name: "Angular",
+    img: angular,
+    style: {
+      backgroundColor: "#DD0031",
+      color: "#fff",
+    },
+  },
+  {
+    name: "AWS",
+    img: aws,
+    style: {
+      backgroundColor: "#FF9900",
+      color: "#fff",
+    },
+  },
+];
